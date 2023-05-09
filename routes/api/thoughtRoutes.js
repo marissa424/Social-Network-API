190f131909af10d38e74b4722dc40ed033bfc4f0
Marissa T <marissalynnthompson@gmail.com>
2023-05-09 14:55:42 -0600

    added user and thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  createThought,
+  getOneThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thoughtControllers');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
